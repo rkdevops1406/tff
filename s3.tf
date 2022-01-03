8101f4b04c97777bf7b753caa5eca7deae0e653d
santhoshkumarmrpg <95461998+santhoshkumarmrpg@users.noreply.github.com>
2022-01-03 09:46:58 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucklebuck" {
-  bucket = "my-tf-massiveprince-bucket11"
+  bucket = "santhosh-test-aws-123-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "rav1i-day1a-santhos1h-deepak1231"
+    Name        = "test"
     Environment = "Dev"
   }
 }
